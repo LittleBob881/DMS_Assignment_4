270543e60d018a8385711afd77898c3085feca3a
Bernadette Cruz <berniemaecruz@gmail.com>
2021-06-09 12:58:02 +1200

    git ignore working

--- KPopProfileService/src/java/KPopProfileService/ApplicationConfig.java
+++ KPopProfileService/src/java/KPopProfileService/ApplicationConfig.java
@@ -22,5 +22,7 @@ public class ApplicationConfig extends Application {
     }
 
     private void addRestResourceClasses(Set<Class<?>> resources) {
+        resources.add(KPopProfileService.FavouriteResource.class);
+        resources.add(KPopProfileService.ProfileResource.class);
     }
 }
