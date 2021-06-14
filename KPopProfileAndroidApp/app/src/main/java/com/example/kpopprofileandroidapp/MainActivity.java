47db77d83d7c85af5ce6915d6678986831f6efb8
Bernadette Cruz <berniemaecruz@gmail.com>
2021-06-14 21:44:31 +1200

    rebase

--- KPopProfileAndroidApp/app/src/main/java/com/example/kpopprofileandroidapp/MainActivity.java
+++ KPopProfileAndroidApp/app/src/main/java/com/example/kpopprofileandroidapp/MainActivity.java
@@ -1,22 +1,41 @@
 package com.example.kpopprofileandroidapp;
 
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Color;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
+import android.nfc.NfcAdapter;
+import android.nfc.NfcEvent;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toolbar;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.NavigationUI;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import org.w3c.dom.Text;
+
 /*
     This Activity acts as a listener for the bottom navigation, which uses the NavController to navigate between different fragments
  */
-public class MainActivity extends AppCompatActivity {
+@RequiresApi(api = Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+public class MainActivity extends AppCompatActivity implements NfcAdapter.CreateNdefMessageCallback{
     BottomNavigationView bottomNavigationView;
     String username = "";
+    private NfcAdapter nfcAdapter;
+    private TextView nfcStatusTextView;
+    private TextView nfcResponseView;
+    private TextView sendFriendRequestButton;
+    private PendingIntent pendingIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,7 +54,82 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_fragment);
         NavigationUI.setupWithNavController(bottomNavigationView, navController);
         NavigationUI.setupActionBarWithNavController(this, navController);
+
+        //check if NFC feature available on phone
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        nfcStatusTextView = findViewById(R.id.nfcStatusTextView);
+        if(nfcAdapter == null)
+        {
+            sendFriendRequestButton.setEnabled(false);
+//            nfcStatusTextView.setTextColor(Color.RED);
+//            nfcStatusTextView.setText("NFC not available for this device!");
+        }
+        else if(nfcAdapter != null)
+        {   sendFriendRequestButton = findViewById(R.id.sendFriendNFCButton);
+//            nfcStatusTextView.setTextColor(Color.GREEN);
+//            nfcStatusTextView.setText("NFC available for this device!");
+            //sendFriendRequestButton.setOnClickListener(this);
+
+        }
+
+        pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass())
+                .addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
     }
 
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
 
+        nfcAdapter.enableForegroundDispatch(this, pendingIntent, null, null);
+        nfcAdapter.setNdefPushMessageCallback(this, this);
+       // nfcAdapter.setNdefPushMessage(createNdefMessage(), this);
+        System.out.println("~~~~~~~~~~~~~ ON RESUME!!!");
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+    @Override
+    public NdefMessage createNdefMessage(NfcEvent event) {
+
+        NdefMessage message = new NdefMessage(new NdefRecord[]
+                {NdefRecord.createApplicationRecord("HELLO BRITNEY BITCH")
+                });
+        System.out.println("!!!!!!!!!!!!!!!!!! NDEF MESSAGE CREATED");
+
+        return message;
+    }
+
+    // displaying message
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        NdefMessage[] messages = getNdefMessages(intent);
+       // nfcStatusTextView.setText(displayByteArray(messages[0].toByteArray()));
+        System.out.println(displayByteArray(messages[0].toByteArray()));
+        System.out.println("~~~~~~~~~~~~~ DISPLAY RESUME!!!");
+    }
+
+    static String displayByteArray(byte[] bytes) {
+        String res="";
+        StringBuilder builder = new StringBuilder().append("[");
+        for (int i = 0; i < bytes.length; i++) {
+            res+=(char)bytes[i];
+        }
+        return res;
+    }
+
+    private NdefMessage[] getNdefMessages(Intent intent) {
+        System.out.println("~~~~~~~~~~~~~ GET RAW ~~~~~~~~~~~~");
+        Parcelable[] rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+        if (rawMessages != null) {
+            NdefMessage[] messages = new NdefMessage[rawMessages.length];
+            for (int i = 0; i < messages.length; i++) {
+                messages[i] = (NdefMessage) rawMessages[i];
+            }
+            return messages;
+        } else {
+            return null;
+        }
+    }
 }
--- KPopProfileAndroidApp/app/src/main/java/com/example/kpopprofileandroidapp/NfcFragment.java
+++ KPopProfileAndroidApp/app/src/main/java/com/example/kpopprofileandroidapp/NfcFragment.java
@@ -1,9 +1,13 @@
 package com.example.kpopprofileandroidapp;
 
 import android.graphics.Color;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -11,6 +15,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+
+import javax.security.auth.callback.Callback;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -22,9 +28,6 @@ import android.widget.TextView;
 public class NfcFragment extends Fragment implements View.OnClickListener {
 
     Button sendFriendRequestButton;
-    private NfcAdapter nfcAdapter;
-    private TextView nfcStatusTextView;
-    private TextView nfcResponseView;
 
     public NfcFragment() {
         // Required empty public constructor
@@ -47,32 +50,15 @@ public class NfcFragment extends Fragment implements View.OnClickListener {
 
         // Inflate the layout for this fragment
         View view =  inflater.inflate(R.layout.fragment_nfc, container, false);
-        sendFriendRequestButton = view.findViewById(R.id.sendFriendNFCButton);
-        nfcStatusTextView = view.findViewById(R.id.nfcStatusTextView);
-
-        //check if NFC feature available on phone
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this.getContext());
-        if(nfcAdapter == null)
-        {
-            sendFriendRequestButton.setEnabled(false);
-            nfcStatusTextView.setTextColor(Color.RED);
-            nfcStatusTextView.setText("NFC not available for this device!");
-        }
-        else
-        {
-            nfcStatusTextView.setTextColor(Color.RED);
-            nfcStatusTextView.setText("NFC not available for this device!");
-            sendFriendRequestButton.setOnClickListener(this);
-        }
-
-
 
         return view;
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.ICE_CREAM_SANDWICH)
     @Override
     public void onClick(View v) {
         //TODO: add NFC handler for ANDROID BEAM
+
 
     }
 }
